a029bfd56fe892a219b7a62e5a009ba24561336d
Miguel Serrano <mserranom@users.noreply.github.com>
2026-02-20 10:34:10 +0100

    [filestore] add `pdftocairo` conversion option (#31627)
    Includes patches for Server Pro/CE 5.x and 6.x
    
    GitOrigin-RevId: 67e387c96421b681339dbc1d89a8af0c34a163ef

--- server-ce/config/settings.js
+++ server-ce/config/settings.js
@@ -478,6 +478,8 @@ switch (process.env.OVERLEAF_FILESTORE_BACKEND) {
     }
 }
 
+settings.converter = process.env.CONVERTER || 'pdftocairo'
+
 if (
   !settings.trustedProxyIps.includes('loopback') &&
   !settings.trustedProxyIps.includes('localhost') &&
